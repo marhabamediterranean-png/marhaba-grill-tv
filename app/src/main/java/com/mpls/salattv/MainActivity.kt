5bcf9ce154b7e82cef4c3e660c7111a8d7412ecb
marhabamediterranean-png <marhabamediterranean@gmail.com>
2026-06-26 12:31:17 -0500

    Add files via upload

--- app/src/main/java/com/mpls/salattv/MainActivity.kt
+++ app/src/main/java/com/mpls/salattv/MainActivity.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
